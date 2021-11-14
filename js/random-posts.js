64db55cc60d988fd7d3ba719ea67cee623ac5d89
rivanimarion <84041926+rivanimarion@users.noreply.github.com>
2021-11-14 12:32:18 -0500

    Update random-posts.js

--- js/random-posts.js
+++ js/random-posts.js
@@ -41,7 +41,7 @@ function random_posts(json) {
         document.write('<a href="' + randompostsurl + '" rel="nofollow"><img alt="' + randompoststitle + '" src="' + randompoststhumb + '"/></a>');
         document.write('<div><a href="' + randompostsurl + '" rel="nofollow">' + randompoststitle + '</a></div>');
         if (randomposts_details == 'yes') {
-            document.write('<span><div  class="random-info">' + randomposts_date.substring(8, 10) + '.' + randomposts_date.substring(5, 7) + '.' + randomposts_date.substring(0, 4) + ' - ' + randomposts_commentsnum) + ' - '+<data:post.labels.last.name/> + '</div></span>'
+            document.write('<span><div  class="random-info">' + randomposts_date.substring(8, 10) + '.' + randomposts_date.substring(5, 7) + '.' + randomposts_date.substring(0, 4) + ' - ' + randomposts_commentsnum) + '</div></span>'
         };
         document.write('<br/><div class="random-summary">' + randomposts_snippet + '</div><div style="clear:both"></div></li>')
     }
